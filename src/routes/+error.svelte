46763a049c61d6e32cd7afc5091271f5407ee6fa
0x0501 <41354084+0x0501@users.noreply.github.com>
2025-05-01 19:10:59 +0800

    feat: add font-end error page.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<!-- Error Page (error code 404) -->
+
+<script lang="ts">
+	import { page } from '$app/state';
+	import HomeNavigation from '$lib/components/HomeNavigation.svelte';
+
+	import { A, Heading, P } from 'flowbite-svelte';
+</script>
+
+<HomeNavigation />
+
+<svelte:head>
+	<title>Anki workshop | 出错啦</title>
+</svelte:head>
+
+<div class="flex flex-grow items-center justify-center pt-20">
+	<div class="flex flex-col items-center gap-3">
+		<Heading tag="h2" class="text-center">{page.status}</Heading>
+
+		<P>错误（{page.error?.message}）: {page.url.pathname}</P>
+		<A href='/'>返回主页</A>
+	</div>
+</div>
