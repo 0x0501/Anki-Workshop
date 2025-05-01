680c826b76737482e19c138efffbef04dbc22890
0x0501 <41354084+0x0501@users.noreply.github.com>
2025-05-01 15:29:38 +0800

    feat: create db schema; configure Cloudflare D1 and R2.

---
+++ src/lib/database/schema/index.ts
@@ -0,0 +1,59 @@
+import { int, real, sqliteTable, text, index } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+import type { supportPlatformOption } from '$lib/interfaces/supportPlatformOption';
+
+enum UserRole {
+	Root = 1, 
+	Admin = 2,
+	User = 3
+}
+
+export const users = sqliteTable('users', {
+	id: int('id').primaryKey({ autoIncrement: true }),
+	email: text('email').unique().notNull(),
+	username: text('username').unique().notNull(),
+	hashed_password: text('hashed_password').notNull(),
+	created_at: text('created_at')
+		.notNull()
+		.default(sql`(current_timestamp)`),
+	updated_at: text('updated_at')
+		.notNull()
+		.default(sql`(current_timestamp)`)
+		.$onUpdate(() => new Date().toString()),
+	role: int('role').$type<UserRole>().notNull().default(UserRole.User)
+});
+
+export const decks = sqliteTable(
+	'decks',
+	{
+		id: int('id').primaryKey({ autoIncrement: true }),
+		// foreign key linking to user table
+		deck_author_id: int('id').references(() => users.id),
+		deck_name: text('deck_name').notNull(),
+		deck_description: text('deck_description').notNull(),
+		// stored as serialized JSON Blob
+		deck_tags: text('deck_tags'),
+		deck_size: int('deck_size').notNull().default(0),
+		deck_card_count: int('deck_card_count'),
+		deck_price: real('deck_price'),
+		last_updated_date: text('last_updated_date')
+			.notNull()
+			.default(sql`(current_timestamp)`)
+			.$onUpdate(() => new Date().toString()),
+		created_date: text('created_date')
+			.notNull()
+			.default(sql`(current_timestamp)`),
+		is_deck_on_sale: int('is_deck_on_sale', { mode: 'boolean' }).default(false),
+		deck_liked_by_people: int('deck_liked_by_people').default(0),
+		deck_cover_image_url: text('deck_cover_image_url'),
+		// stored as serialized JSON Blob
+		support_platform: text('support_platform')
+			.notNull()
+			.default(
+				JSON.stringify({
+					platform: 'All'
+				} as supportPlatformOption)
+			)
+	},
+	(table) => [index('deck_name_idx').on(table.deck_name)]
+);
