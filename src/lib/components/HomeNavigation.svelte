094aebc9167b0a10569909b0a03c3223892dfbb7
0x0501 <41354084+0x0501@users.noreply.github.com>
2025-05-07 20:29:48 +0800

    feat: Update About page with navigation and content enhancements

--- src/lib/components/HomeNavigation.svelte
+++ src/lib/components/HomeNavigation.svelte
@@ -10,7 +10,7 @@
 
 	let { system_settings } = $props();
 
-	console.warn(system_settings)
+	console.warn(system_settings);
 
 	let session = page.data.session;
 </script>
@@ -31,9 +31,9 @@
 			ulClass="flex flex-col md:flex-row md:items-center gap-2 md:gap-6 py-2 px-2 md:py-2 mt-4 md:mt-0"
 		>
 			<NavLi class="text-sm md:text-base" href="/">卡组</NavLi>
-			<NavLi class="text-sm md:text-base" href="/common-faq">常见问题</NavLi>
-			<NavLi class="text-sm md:text-base" href="/contact">联系</NavLi>
-			<NavLi class="text-sm md:text-base" href="/about">关于</NavLi>
+			<!-- <NavLi class="text-sm md:text-base" href="/common-faq">常见问题</NavLi> -->
+			<!-- <NavLi class="text-sm md:text-base" href="/contact">联系</NavLi> -->
+			<NavLi class="text-sm md:text-base" href="/about">关于本站</NavLi>
 			<div class="flex gap-3 flex-col md:flex-row items-center">
 				{#if session}
 					<span>{session.user?.name}</span>
@@ -54,7 +54,6 @@
 						class="text-center font-medium focus-within:ring-4 focus-within:outline-hidden items-center justify-center px-4 py-2 text-sm text-white bg-primary-700 hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 focus-within:ring-primary-300 dark:focus-within:ring-primary-800 rounded-lg"
 						href="/register">注册</a
 					>
-				{:else}
 					<a
 						class="text-center font-medium focus-within:ring-4 focus-within:outline-hidden items-center justify-center px-4 py-2 text-sm text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 focus-within:ring-gray-200 dark:focus-within:ring-gray-700 rounded-lg inline-flex"
 						color="light"
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,29 @@
-<h1>Anki workshop是一个致力于提供共享牌组的地方</h1>
+<script lang="ts">
+	import HomeNavigation from '$lib/components/HomeNavigation.svelte';
+	import { Blockquote, Footer, Heading, Hr, P } from 'flowbite-svelte';
+
+	let { data } = $props();
+</script>
+
+<HomeNavigation system_settings={data.system_settings} />
+
+<div class="py-12 px-4 flex flex-col items-center gap-3">
+    <img alt="Anki workshop Logo" src="/logo.png" class="size-36 lg:size-64"/>
+    <Heading tag="h4" class="text-center">欢迎来到 Anki Workshop —— 你的高效学习加速器！</Heading>
+    <div class="grow">
+        <P justify>
+        
+            在这里，我们致力于打造高质量、精心设计的 Anki 记忆卡片（Flashcards）和模板，帮助学生、语言学习者、专业人士更轻松、更有系统地掌握知识。无论你是备考医学、法律、外语，还是想要提升自己的技能，Anki Workshop 都为你提供结构清晰、科学记忆的学习资源。
+        </P>
+    
+        <Blockquote class="text-gray-600">
+            "Crafting Smarter Flashcards for Smarter Learning."
+    （为更聪明的学习，打造更智慧的记忆卡片）
+        </Blockquote>
+    </div>
+
+    <Footer class="fixed bottom-0 start-0 px-2 pb-3 text-sm text-gray-500 w-full">
+        <Hr hrClass="h-px my-3 bg-gray-200 border-0 dark:bg-gray-700"/>
+        注意：Anki Workshop 是独立运营的第三方平台，所提供的内容与服务并非由 Anki 官方开发或运营，我们与其无任何隶属关系或商业利益关联。Anki 是 Anki Inc. 的注册商标。
+    </Footer>
+</div>
